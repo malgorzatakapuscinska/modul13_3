5c740e64b1e1fd06746d1e256fca935374cc8757
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-02-14 20:24:31 +0100

    Add first app

--- js/program.js
+++ js/program.js
@@ -1,9 +1,24 @@
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function(){
-	var input = process.stdin.read();
-	
-	if(input !== null) {
-		process.stdout.write(input);
-	}
-	
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if(input !== null) {
+        var instruction = input.trim();
+        switch(instruction) {
+        	case '/node-version':
+        		process.stdout.write('Node version is: ' + process.versions.node);
+        		break;
+        	case '/system-language':
+        		process.stdout.write('System language is ' + process.env.lang);
+        		break;
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/sayhello':
+                process.stdout.write('hello!\n');
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
+    }
 });
